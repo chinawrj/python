db6f11c22223947e7186d6d5eeb43af90d660fbf
Rujun Wang <chinawrj@gmail.com>
2017-06-02 09:52:38 +0800

    first version with fully page one parse

--- web/ipo/test.py
+++ web/ipo/test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import requests
+import re
 from bs4 import BeautifulSoup
 
 response = requests.get('http://ipo.csrc.gov.cn/infoByFileType.action?pageNo=1&temp=&fileType=1&flag=single&blockType=prepare')
@@ -7,6 +8,19 @@ soup = BeautifulSoup(response.text, "html.parser")
 
 tableHeader = soup.find("tr", attrs={"class":"fontWeight"})
 tableItems = soup.find_all("tr", attrs={"class":"typeborder"})
+totalFooter = soup.find("div", attrs={"class":"page"})
+
+totalPageCountContainer = totalFooter.span
+for pageLink in totalPageCountContainer.find_all("a"):
+    if "尾页" == pageLink.get_text() :
+        pageLastLink = pageLink['href']
+        totalPageCount = (re.findall("pageNo=(\d+)", pageLastLink))[0]
+print("总页数:",  totalPageCount)
+
+totalFooter.span.decompose() #remove span
+totalCountContainer = "".join(totalFooter.get_text().split())
+totalCount = re.findall("(\d+)条记录", totalCountContainer)
+print("总数目:",  totalCount)
 
 headings = [th.get_text().strip() for th in tableHeader.find_all("td")]
 datasets = []
